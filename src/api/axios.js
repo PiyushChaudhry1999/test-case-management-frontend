da4417fceb242e74d4838b113e6c08825d4b90c8
Piyush Chaudhry <piyushchaudhry2020@gmail.com>
2026-01-05 15:24:10 +0530

    Update API URL to Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://test-case-management-backend.onrender.com/api", // production backend
 });
 
 api.interceptors.request.use((config) => {
