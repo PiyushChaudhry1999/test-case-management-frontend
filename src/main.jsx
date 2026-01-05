c518eb46c05bb31aee344aa265e91152fa7fffe2
Piyush Chaudhry <piyushchaudhry2020@gmail.com>
2026-01-05 11:29:44 +0530

    Prepare GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { AuthProvider } from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/test-case-management-frontend">
       <AuthProvider>
         <App />
       </AuthProvider>
